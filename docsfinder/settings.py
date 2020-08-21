1fbf4a98a98e87ff256b65ce64ac378786f813da
sudharsan004 <sudharsansriram8921@gmail.com>
2020-08-21 20:43:18 +0530

    added sitemap

--- docsfinder/settings.py
+++ docsfinder/settings.py
@@ -38,8 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'django.contrib.sitemaps',
+    'django.contrib.sites',
+    
     'find.apps.FindConfig',
 ]
+
+SITE_ID=1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
