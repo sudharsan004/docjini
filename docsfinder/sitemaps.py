586f3a416af11328375fcd735b3c027732908b92
sudharsan004 <sudharsansriram8921@gmail.com>
2020-08-28 14:39:24 +0530

    edit sitemap

--- docsfinder/sitemaps.py
+++ docsfinder/sitemaps.py
@@ -1,5 +1,7 @@
 from django.contrib import sitemaps
 from django.urls import reverse
+from django.contrib.sites.models import Site
+
 
 class StaticViewSitemap(sitemaps.Sitemap):
     priority = 0.5
@@ -7,7 +9,11 @@ class StaticViewSitemap(sitemaps.Sitemap):
 
     def items(self):
         return ['home', 'how-to', 'contact','terms',
-        'privacy',]
+        'privacy']
 
     def location(self, item):
         return reverse(item)
+    
+    def get_urls(self, site=None, **kwargs):
+        site = Site(domain='3.129.240.172', name='3.129.240.172')
+        return super(StaticViewSitemap, self).get_urls(site=site, **kwargs)
